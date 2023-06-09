b42b49b38bf46f7d401609a8849458d1289f6390
Ibrahim <suleimanonnet@gmail.com>
2023-06-09 09:40:16 +0100

    Bug fixes and improvements

--- packages/react-app/components/EditProductModal.tsx
+++ packages/react-app/components/EditProductModal.tsx
@@ -58,7 +58,7 @@ const EditProductModal = ({ id, product }: Product) => {
 
   // Convert the product price to wei
   const productPriceInWei = ethers.utils.parseEther(
-    debouncedProductPrice.toString()
+    `${debouncedProductPrice.toString() || 0}`
   );
 
   // Use the useContractSend hook to use our editProduct function on the marketplace contract and add a product to the marketplace
